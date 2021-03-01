f7cc6baa6fba353d0cb0a91d1fca344c63f4ddc2
ChihuahuaMH <40608845+ChihuahuaMH@users.noreply.github.com>
2021-03-01 18:06:21 +0800

    Create Hashmat the Brave Warrior.cpp
    求兩數相差

---
+++ Hashmat the Brave Warrior.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long int hashmat, oppoArmy;
+  while (cin >> hashmat >> oppoArmy) {
+    cout << abs(hashmat - oppoArmy) << endl;
+  }
+
+  return 0;
+}
